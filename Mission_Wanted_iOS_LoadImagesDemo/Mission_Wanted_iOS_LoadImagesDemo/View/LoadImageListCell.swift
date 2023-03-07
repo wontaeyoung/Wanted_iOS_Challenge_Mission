6f7364a5586e4e27f1141f83728371e51e9a05bb
Taeyoung Won <hexter1994@gmail.com>
2023-03-07 20:04:25 +0900

    [Chore] 뷰 프로퍼티 접근 제어자 pulbic으로 변경

--- Mission_Wanted_iOS_LoadImagesDemo/Mission_Wanted_iOS_LoadImagesDemo/View/LoadImageListCell.swift
+++ Mission_Wanted_iOS_LoadImagesDemo/Mission_Wanted_iOS_LoadImagesDemo/View/LoadImageListCell.swift
@@ -26,9 +26,10 @@ fileprivate enum ImageURL {
 
 class LoadImageListCell: UIView {
 
-    private let loadedImageView = UIImageView()
-    private let loadProgressBar = UIProgressView(progressViewStyle: .bar)
-    private let loadButton = UIButton()
+    let loadedImageView = UIImageView()
+    let loadProgressBar = UIProgressView(progressViewStyle: .bar)
+    let loadButton = UIButton()
+    private let hStack = UIStackView()
     private var observation: NSKeyValueObservation?
     
     override init(frame: CGRect) {
@@ -36,7 +37,6 @@ class LoadImageListCell: UIView {
         // 셋업 메서드 나열
         reset()
     }
-    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -46,16 +46,19 @@ class LoadImageListCell: UIView {
         setupImageView()
         setupProgressBar()
         setupButton()
+        setupStack()
     }
     
     private func setupImageView() {
         let defaultImage = UIImage(systemName: "photo")
         loadedImageView.image = defaultImage
+//        self.addSubview(loadedImageView)
     }
     
     private func setupProgressBar() {
         loadProgressBar.progressTintColor = .blue
         loadProgressBar.progress = 0
+//        self.addSubview(loadProgressBar)
     }
     
     private func setupButton() {
@@ -63,14 +66,28 @@ class LoadImageListCell: UIView {
         loadButton.setTitle("Loading...", for: .selected)
         loadButton.setTitleColor(.white, for: .normal)
         loadButton.backgroundColor = .blue
-        loadButton.isEnabled = true
         
         loadButton.addTarget(self, action: #selector(loadImage), for: .touchUpInside)
+//        self.addSubview(loadButton)
+    }
+    
+    private func setupStack() {
+        hStack.axis = .horizontal
+        hStack.distribution = .fill
+        hStack.alignment = .center
+        hStack.spacing = 5
+        
+        hStack.addArrangedSubview(loadedImageView)
+        hStack.addArrangedSubview(loadProgressBar)
+        hStack.addArrangedSubview(loadButton)
+        self.addSubview(hStack)
+        hStack.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
     
     @objc private func loadImage(_ sender: UIButton) {
         sender.isSelected = !sender.isSelected
-        
         
         guard (0...4).contains(sender.tag) else { return }
         
